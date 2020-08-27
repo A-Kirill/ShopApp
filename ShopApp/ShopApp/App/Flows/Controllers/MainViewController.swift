7267ffbd7741ac703d43c98e4f0949610489113b
Kirill Anisimov <anisimov_k@icloud.com>
2020-08-27 11:50:17 +0300

    added gradient view and button logout

--- ShopApp/ShopApp/App/Flows/Controllers/MainViewController.swift
+++ ShopApp/ShopApp/App/Flows/Controllers/MainViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    let requestFactory = RequestFactory()
+    
     @IBOutlet weak var showCatalog: UIButton!
     @IBOutlet weak var myBasket: UIButton!
     @IBOutlet weak var editProfile: UIButton!
@@ -17,10 +19,26 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let logOut = UIBarButtonItem(title: "Logout", style: UIBarButtonItem.Style.plain, target: self, action: #selector(tryLogout))
+        self.navigationItem.leftBarButtonItem = logOut
 
         showCatalog.layer.cornerRadius = 30
         myBasket.layer.cornerRadius = 30
         editProfile.layer.cornerRadius = 30
     }
 
+    @objc func tryLogout() {
+        self.navigationController?.popViewController(animated: true)
+        
+        let logout = requestFactory.makeLogoutRequestFatory()
+        logout.logout(idUser: 123) { response in
+            switch response.result {
+            case .success(let logout):
+                print(logout)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
